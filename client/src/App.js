40baf718c4dd8b79283cc83a92b968722f8af870
tsevedire <tsevedire@hotmail.com>
2020-05-15 10:54:41 -0400

    commit crisss

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState();
+
+    const handleClick = async() => {
+        const res = await axios.get('http://localhost:9000/users');
+        setData(res.data);
+        console.log("allo" + res.data);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save crissss to reload.
+                </p>
+                <button onClick={handleClick}>load Data</button>
+                <p>{data}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
