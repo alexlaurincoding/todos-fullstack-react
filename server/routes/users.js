f1e61e7816cfdb01082ed8a596972f0f809c8e7a
tsevedire <tsevedire@hotmail.com>
2020-05-15 11:05:39 -0400

    commit message serveur

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Ca marche tu la??');
 });
 
 module.exports = router;
