30cec7e5df6b0291f0000e1e3af6f4b22b8f4f14
tsevedire <tsevedire@hotmail.com>
2020-05-22 12:00:32 -0400

    commit creermabranche

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,25 @@ import React ,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
 
     const handleClick = async() => {
-        const res = await axios.get('http://localhost:9000/users');
+        const res = await axios.get('http://localhost:9000/api/todos');
         setData(res.data);
-        console.log("allo" + res.data);
+        console.log(res.data);
     }
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> CRISSSSSSS toi-meme!!!!
-                </p>
-                <button onClick={handleClick}>load Data</button>
-                <p>{data}</p>
+                <Button onClick={handleClick}>Afficher les choses a faires</Button>
+                <div>
+                    {data.map(todo => <p>{todo.titre}{"    "}<Button>Delete</Button></p>)}
+                </div>
             </header>
         </div>
     );
--- server/api.js
+++ server/api.js
@@ -25,6 +25,16 @@ router.post("/todos", async  (req, res) => {
     }
 })
 
+router.get("/todos/:_id", async  (req, res) => {
+    try {
+        const todo = await Todo.findOne({_id: req.params._id}).orFail()
+        res.send(todo)
+    }catch{
+        res.statut(404)
+        res.send({error: "Todo not found"})
+    }
+})
+
 router.delete("/todos/:_id", async  (req, res) => {
     try {
         await Todo.findByIdAndDelete(req.params._id);
